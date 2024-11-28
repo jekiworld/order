024d336dbd9f7f0e609552f9003f659348271e79
Bakhredin <bax.2002@mail.ru>
2024-11-29 00:36:44 +0500

    reacat

--- src/page/Practics/Practice.jsx
+++ src/page/Practics/Practice.jsx
@@ -1,32 +1,11 @@
-import React, { useEffect, useState } from 'react'
+import Welcome from "./components/Welcome";
 
-export default function Practice() {
-
-    const [cart, setCart] = useState([
-        { id: 1, name: 'Laptop', price: 1000 },
-        { id: 2, name: 'Phone', price: 500 },
-        { id: 3, name: 'Tablet', price: 300 },
-    ]);
-
-    const cartId = 2;
-
-    const handleClick = () => {
-        setCart(cart.map(cart => {
-            if (cart.id === cartId) {
-                return { ...cart, price: cart.price * 10 }
-            }
-            else {
-                return cart;
-            }
-        }))
-    }
-
-
-    useEffect(() => { console.log(cart.map(cart => cart.price)) }, [cart])
-
+function Practice() {
     return (
         <div>
-            <button onClick={handleClick}>press</button>
+            <Welcome name="asd"/>
         </div>
-    )
+    );
 }
+
+export default Practice
